e870ecc7b76632eef4b86bc1fc4315032aa4d694
liuu-world <66379367+liuu-world@users.noreply.github.com>
2021-04-01 17:50:01 +0200

    Update ThumbnailServer.php

--- src/ThumbnailServer.php
+++ src/ThumbnailServer.php
@@ -7,9 +7,13 @@ class ThumbnailServer
 {
     public function thumb($path,$type,$size)
     {
-        if (File::exists("/storage/public/{$type}/{$size}/" . $path))
-            return url("/storage/public/{$type}/{$size}/" . $path);
+        if( $path != null) {
+            if (Storage::disk('public')->exists("/public/$type/$size/" . $path))
+                return Storage::disk('public')->url("/public/$type/$size/" . $path);
+            else
+                return url("https://via.placeholder.com/400x300.png");
+        }
         else
-            return url("https://via.placeholder.com/400");
+            return url("https://via.placeholder.com/400x300.png");
     }
 }
