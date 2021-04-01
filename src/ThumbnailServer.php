b042d67a61e4271caa7a2622e0a5448b5374ac11
liuu-world <66379367+liuu-world@users.noreply.github.com>
2021-04-01 15:59:01 +0200

    Update ThumbnailServer.php

--- src/ThumbnailServer.php
+++ src/ThumbnailServer.php
@@ -9,6 +9,6 @@ class ThumbnailServer
         if (File::exists("/storage/public/{$type}/{$size}/" . $path))
             return url("/storage/public/{$type}/{$size}/" . $path);
         else
-            return '<svg class="bd-placeholder-img" width="400" height="300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">';
+            return url("https://via.placeholder.com/400");
     }
 }
