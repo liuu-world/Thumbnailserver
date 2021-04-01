8369f874acf621c5903f5a0ba4e099b964c8eabf
liuu-world <66379367+liuu-world@users.noreply.github.com>
2021-04-01 15:43:26 +0200

    Create ThumbnailServer.php

---
+++ src/ThumbnailServer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GregBrown\ThumbnailServer;
+
+class ThumbnailServer
+{
+    public function thumb($path,$type,$size)
+    {
+        if (File::exists("/storage/public/{$type}/{$size}/" . $path))
+            return url("/storage/public/{$type}/{$size}/" . $path);
+    }
+}
