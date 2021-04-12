9902ec67ed1b5148b17887c35fc0f21fcd83b31e
liuu-world <66379367+liuu-world@users.noreply.github.com>
2021-04-12 18:54:21 +0200

    Update ThumbnailServer.php

--- src/ThumbnailServer.php
+++ src/ThumbnailServer.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Storage;
 
 class ThumbnailServer
 {
-    public function thumb($filename,$type,$size)
+    public function thumb($filename,$type,$size,$placeHolderX='420',$placeHolderY='280')
     {
+        $placeHolderX != null ? $x = $placeHolderX : $x = '420';
+        $placeHolderY != null ? $y = $placeHolderY : $x = '280';
         if( $filename != null) {
             if (Storage::disk('public')->exists("/$type/$size/" . $filename))
                 return Storage::disk('public')->url("/$type/$size/" . $filename);
             else
-                return url("https://via.placeholder.com/400x300.png");
+                return url("https://via.placeholder.com/{$x}x{$y}.png");
         }
         else
-            return url("https://via.placeholder.com/400x300.png");
+            return url("https://via.placeholder.com/{$x}x{$y}.png");
     }
 }
